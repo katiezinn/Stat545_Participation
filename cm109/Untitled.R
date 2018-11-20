a5bbeb60b0804d6a5cba905ebb0bf68cb2ac2f13
Katie Zinn <katiezinn@gmail.com>
2018-11-20 10:22:07 -0800

    adding to the folder

--- cm109/Untitled.R
+++ cm109/Untitled.R
@@ -1,1 +1,7 @@
- 
+#this is where we will code our make code for make
+#want to display spaces so we can format roperly
+#make sure tabs are not spaces also in preferences
+
+
+plot.png: foo.R
+  Rscript foo.R
